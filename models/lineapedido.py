0c3f4f3902c7191a2541c80d31cbecd50de361e2
Etutam1 <matute1172@gmail.com>
2024-02-07 14:22:01 +0100

    update 7/2

--- models/lineapedido.py
+++ models/lineapedido.py
@@ -3,10 +3,17 @@ from odoo.exceptions import ValidationError
 
 
 class lineapedido(models.Model):
-    _name = 'odoo_basico.lineapedido.xml'
+    _name = 'odoo_basico.lineapedido'
     _description = 'exemplo linea pedido'
+    _order = 'name asc'
+
     name = fields.Char(required=True, size=20, string="Linea Pedido:")
+    descripcion = fields.Text(string="Descripción da Línea Pedido:")
 
     # Os campos One2many Non se almacenan na BD
-    pedido_id = fields.Many2one('odoo_basico.pedido',
-                                ondelete="cascade", required=True)
+    pedido_id = fields.Many2one('odoo_basico.pedido', ondelete="cascade", required=True)
+
+    informacion_ids = fields.Many2many("odoo_basico.informacion",
+                                       string="Rexistro de Información",
+                                       relation="odoo_basico_lineapedido_informacion",
+                                       column1="lineapedido_id", column2="informacion_id")
