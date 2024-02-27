18ffcc321bf6bad91d4e38604729ca95c0b9b2ac
Etutam1 <matute1172@gmail.com>
2024-02-27 14:26:59 +0100

    update 27/2

--- models/informacion.py
+++ models/informacion.py
@@ -1,6 +1,6 @@
-
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+from odoo.tools.safe_eval import pytz
 
 
 class informacion(models.Model):
@@ -14,39 +14,48 @@ class informacion(models.Model):
     alto_en_cms = fields.Integer(string="Alto en centímetros:")
     longo_en_cms = fields.Integer(string="Longo en centímetros:")
     ancho_en_cms = fields.Integer(string="Ancho en centímetros:")
-    volume = fields.Float(digits=(6,6), compute="_volume", store=True, string= "Volume m3")
+    volume = fields.Float(digits=(6, 6), compute="_volume", store=True, string="Volume m3")
     literal = fields.Char(store=False)
-    peso = fields.Float(digits=(6,2), default =2.7, string="Peso en KG.s/m3:")
+    peso = fields.Float(digits=(6, 2), default=2.7, string="Peso en KG.s/m3:")
     densidade = fields.Float(compute="_densidade", store=True, string="Densidade en KG/m3:")
-    autorizado = fields.Boolean(default = True, string="¿Autorizado?")
-    sexo_traducido = fields.Selection([('Hombre','Home'), ('Mujer','Muller'), ('Otros','Outros')], string="Sexo:")
+    autorizado = fields.Boolean(default=True, string="¿Autorizado?")
+    sexo_traducido = fields.Selection([('Hombre', 'Home'), ('Mujer', 'Muller'), ('Otros', 'Outros')], string="Sexo:")
 
     foto = fields.Binary(string='Foto')
     adxunto_nome = fields.Char(string="Nome Adxunto")
     adxunto = fields.Binary(string="Arquivo adxunto")
 
-    #os campos Man2one crean un campo na BD
+    # os campos Man2one crean un campo na BD
     moeda_id = fields.Many2one('res.currency', domain="[('position','=','after')]")
 
     # con domain, filtramos os valores mostrados. Pode ser mediante unha constante (vai entre comillas) ou unha variable
-    moeda_euro_id = fields.Many2one('res.currency', default=lambda self: self.env['res.currency'].search([('name', '=', "EUR")], limit=1))
+    moeda_euro_id = fields.Many2one('res.currency',
+                                    default=lambda self: self.env['res.currency'].search([('name', '=', "EUR")],
+                                                                                         limit=1))
     gasto_en_euros = fields.Monetary("Gasto en Euros", 'moeda_euro_id')
     moeda_en_texto = fields.Char(related="moeda_id.currency_unit_label", string="Moeda en formato texto", store=True)
     creador_da_moeda = fields.Char(related="moeda_id.create_uid.login", string="Usuario creador da moeda", store=True)
-
+    data = fields.Date(string="Data", default=lambda self: fields.Date.today())
+    data_hora = fields.Datetime(string="Data e Hora", default=lambda self: fields.Datetime.now())
+    hora_utc = fields.Char(compute="_hora_utc", string="Hora UTC", size=15, store=True)
+    hora_actual = fields.Char(compute="_hora_actual", string="Hora Actual", size=15, store=True)
+    hora_timezone_usuario = fields.Char(compute="_hora_timezone_usuario", string="Hora Timezone do Usuario", size=15,
+                                        store=True)
 
     @api.depends('alto_en_cms', 'longo_en_cms', 'ancho_en_cms')
     def _volume(self):
         for rexistro in self:
-            rexistro.volume = (float(rexistro.alto_en_cms) * float(rexistro.longo_en_cms) * float(rexistro.ancho_en_cms))/ 1000000
+            rexistro.volume = (float(rexistro.alto_en_cms) * float(rexistro.longo_en_cms) * float(
+                rexistro.ancho_en_cms)) / 1000000
 
     @api.depends('volume', 'peso')
     def _densidade(self):
         for rexistro in self:
             if rexistro.volume != 0:
-             rexistro.densidade = float(rexistro.peso) / float(rexistro.volume)
+                rexistro.densidade = float(rexistro.peso) / float(rexistro.volume)
             else:
                 rexistro.densidade = 0
+
     @api.onchange('alto_en_cms')
     def _avisoAlto(self):
         for rexistro in self:
@@ -80,3 +89,42 @@ class informacion(models.Model):
         mail_id = self.env['mail.mail'].create(mail_valores)
         mail_id.sudo().send()
         return True
+
+    @api.depends('data_hora')
+    def _hora_utc(self):
+        for rexistro in self:  # A hora se almacena na BD en horario UTC (2 horas menos no verán, 1 hora menos no inverno)
+            rexistro.hora_utc = rexistro.data_hora.strftime("%H:%M:%S")
+
+    def actualiza_hora_actual_UTC(
+            self):  # Esta función é chamada dende un boton de informacion.xml e dende _hora_actual
+        for rexistro in self:
+            rexistro.hora_actual = fields.Datetime.now().strftime("%H:%M:%S")
+        # Grava a hora en UTC, se quixesemos poderiamos usar a función  _convirte_data_hora_de_utc_a_timezone_do_usuario
+
+    @api.depends('data_hora')
+    def _hora_actual(self):
+        for rexistro in self:
+            rexistro.actualiza_hora_actual_UTC()
+
+        # Esta función será chamada dende a función actualiza_hora_timezone_usuario_dende_boton_e_apidepends e
+        #  dende pedido.py (Cando insertamos os valores do template self.env.user.tz non ten o timezone do usuario por iso se carga coa hora UTC,
+        #  o botón en pedido.py é para actualizar todos os rexistros masivamente dende outro modelo)
+
+    def actualiza_hora_timezone_usuario(self, obxeto_rexistro):
+        obxeto_rexistro.hora_timezone_usuario = self.convirte_data_hora_de_utc_a_timezone_do_usuario(obxeto_rexistro.data_hora).strftime("%H:%M:%S")  # Convertimos a hora de UTC a hora do timezone do usuario
+
+    def convirte_data_hora_de_utc_a_timezone_do_usuario(self, data_hora_utc_object):  # recibe a data hora en formato object
+        usuario_timezone = pytz.timezone(self.env.user.tz or 'UTC')  # obter a zona horaria do usuario. Ollo!!! nas preferencias do usuario ten que estar ben configurada a zona horaria
+        return pytz.UTC.localize(data_hora_utc_object).astimezone(usuario_timezone)  # hora co horario do usuario en formato object
+        # para usar  pytz temos que facer  import pytz
+
+    def actualiza_hora_timezone_usuario_dende_boton_e_apidepends(
+            self):  # Esta función é chamada dende un boton de informacion.xml e dende @api.depends _hora_timezone_usuario
+        self.actualiza_hora_timezone_usuario(
+            self)  # leva self como parametro por que actualiza_hora_timezone_usuario ten 2 parametros
+        # porque usamos tamén actualiza_hora_timezone_usuario dende outro modelo (pedido.py) e lle pasamos como parámetro o obxeto_rexistro
+    @api.depends('data_hora')
+    def _hora_timezone_usuario(self):
+        for rexistro in self:
+            rexistro.actualiza_hora_timezone_usuario_dende_boton_e_apidepends()
+
